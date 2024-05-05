5cc75798986093a5b038497098c6f633745c8e31
Taro Serigano <taroserigano@gmail.com>
2024-05-05 14:28:29 -0500

    Create useTimeout.js

---
+++ custom-hooks/useTimeout.js
@@ -0,0 +1,64 @@
+
+
+
+
+
+
+
+
+
+
+
+import {useState, useEffect, useRef} from "react" 
+
+const useTimeout =(callback, delay) => { 
+
+    const savedCallback = useRef() 
+
+    // update callback 
+    useEffect(()=> { 
+        savedCallback.current = callback
+    }, [callback])
+
+    useEffect(()=> { 
+        const func = () => savedCallback.current() 
+
+        if(delay){ 
+
+            let timeoutId = setTimeout(func, delay) 
+            return ()=> clearTimeout(timeoutId)
+        }
+    }, [delay]) 
+
+}
+
+export default useTimeout
+
+
+
+
+
+
+// const UseStateBasics = () => {
+
+//   const [val, setVal] = useState(false) 
+
+//   useTimeout(()=> { 
+//     setVal("hello")
+//   }, 1000)
+
+//   return ( 
+//     <> 
+//     {val && <p>Hello</p>}
+//     </>
+//   )
+  
+
+// };
+
+
+// export default UseStateBasics;
+
+
+
+
