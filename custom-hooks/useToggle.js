f066b100e8d7b114f9f68b22c7837c821946ad07
Taro Serigano <taroserigano@gmail.com>
2024-05-05 13:19:26 -0500

    Create useToggle.js

---
+++ custom-hooks/useToggle.js
@@ -0,0 +1,37 @@
+import {useState} from "react" 
+
+const useToggle = (val) => {
+
+    const [value, setValue] = useState(val) 
+
+    const toggle = () => { 
+        setValue(!value) 
+    } 
+
+    return [value, toggle]
+
+ }
+
+
+export default useToggle
+
+
+// const UseStateBasics = () => {
+
+//   const [value, toggle] = useToggle(false) 
+
+//   return ( 
+//     <> 
+//     <button onClick={toggle}>Toggle</button> 
+//     {value && <p>showing</p>}
+//     </>
+//   )
+  
+
+// };
+
+
+// export default UseStateBasics;
+
+
+
