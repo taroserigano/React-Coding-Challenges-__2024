972b94fc086d7d8db6c01f7f6f56621032353eb3
Taro Serigano <taroserigano@gmail.com>
2024-05-04 22:13:09 -0500

    Update stopWatch.jsx

--- stopWatch.jsx
+++ stopWatch.jsx
@@ -1,4 +1,3 @@
-
 import React, {useState, useEffect} from 'react' 
 import { data} from "../../../data" 
 const url = "https://api.github.com/users" 
@@ -16,60 +15,57 @@ import Pagination from './components/Pagination';
 // import SearchBar from "./SearchBar"
 
 const UseStateBasics = () => {
-  const [posts, setPosts] = useState([]);
-  const [loading, setLoading] = useState(false);
 
-  const [page, setPage] = useState(1);
-  const [postsPerPage, setPostPerPage] = useState(10);
+  const [time, setTime] = useState(0) 
+  const [isRunning, setIsRunning] = useState(false) 
 
-  const url = "https://jsonplaceholder.typicode.com/posts"
+  useEffect(() => { 
+    let intervalId 
 
+    if(isRunning){ 
+      intervalId = setInterval(()=> { 
+        setTime(prev => prev+1)
+      }, 10) 
+    } 
 
+    return ()=> clearInterval(intervalId)
+    
+  }, [isRunning]) 
 
-  const [time, setTime] = useState(0);
+  const hours = Math.floor(time / 360000)
+  const minutes = Math.floor((time % 360000) / 6000) 
+  const seconds = Math.floor((time % 6000) / 100) 
+  const milliseconds = time % 100 
 
-  const [isRunning, setIsRunning] = useState(false);
+  const startAndStop = () => { 
+    setIsRunning((prev)=> !prev) 
+  } 
 
-  useEffect(() => {
-    let intervalId;
+  const reset = () => { 
+    setTime(0) 
+  }
 
-    if (isRunning) {
-      intervalId = setInterval(() => setTime(prev => prev + 1), 10);
-    }
-    return () => clearInterval(intervalId);
-  }, [isRunning, time]);
-
-  const hours = Math.floor(time / 360000);
-  const minutes = Math.floor((time % 360000) / 6000);
-  const seconds = Math.floor((time % 6000) / 100);
-  const milliseconds = time % 100;
-
-  const startAndStop = () => {
-    setIsRunning((prev) => !prev)
-  };
-
-  // Method to reset timer back to 0
-  const reset = () => {
-    setTime(0);
-  };
-
-  return (
-    <div className="stopwatch-container">
-      <p className="stopwatch-time">
-        {hours}:{minutes.toString().padStart(2, "0")}:
+  return ( 
+    <> 
+    <p>
+    {hours}:{minutes.toString().padStart(2, "0")}:
         {seconds.toString().padStart(2, "0")}:
         {milliseconds.toString().padStart(2, "0")}
-      </p>
-      <div className="stopwatch-buttons">
-        <button className="stopwatch-button" onClick={startAndStop}>
-          {isRunning ? "Stop" : "Start"}
-        </button>
-        <button className="stopwatch-button" onClick={reset}>
-          Reset
-        </button>
-      </div>
+    </p>
+    <div> 
+      <button onClick={startAndStop}> 
+      {isRunning ? "Stop" : "Start"} 
+      </button>
+      <button onClick={reset}>
+        Reset
+      </button>
     </div>
-  );
+    
+    </>
+  )
+
+
+
 };
 
 
