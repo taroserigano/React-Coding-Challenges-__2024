d9f9bc1a581f92f6a0a9e2bc7525f71cbd3d40f2
Taro Serigano <taroserigano@gmail.com>
2024-05-30 18:22:29 -0500

    Update StopWatch_BETA.jsx

--- StopWatch_BETA.jsx
+++ StopWatch_BETA.jsx
@@ -1,71 +1,49 @@
-import React, {useState, useEffect, useRef} from 'react' 
-import { data} from "../../../data" 
-// const url = "https://api.github.com/users" 
+import React, { useState, useEffect, useRef } from "react";
+import axios from "axios";
 
-import axios from "axios"
+import Note from "./components/Note";
+import CreateArea from "./components/CreateArea";
 
-import Note from "./components/Note" 
-import LoginForm from './LoginForm';
+import LoginForm from "./LoginForm";
 
 import "./style.css";
 
-import Posts from './components/Posts';
-import Pagination from './components/Pagination';
-import useFocus from  "./components/useFocus"
-import useToggle from "./components/useToggle"
-import useTimeout from "./components/useTimeout"
-// import SearchBar from "./SearchBar"
+import Posts from "./components/Posts";
+import Pagination from "./components/Pagination";
+import useFocus from "./components/useFocus";
+import useToggle from "./components/useToggle";
+import useTimeout from "./components/useTimeout";
+import useArray from "./components/useArray";
+import itemsData from "./items.json";
 
-import itemsData from "./items.json"
-const url = "https://jsonplaceholder.typicode.com/posts"
-
+const url = "https://jsonplaceholder.typicode.com/posts";
 
 const UseStateBasics = () => {
+  const [time, setTime] = useState(0);
+  const [count, setCount] = useState(0);
 
-  const [time,setTime] = useState(0)
-  const [counting, setCounting] = useState(false)
-  
-  const ref = useRef(null) 
+  const ref = useRef(null);
 
-  useEffect(()=> { 
+  const run = () => {
+    ref.current = setInterval(() => {
+      setCount(count + 1);
+    }, 100);
+  };
 
-    if(counting){
-    ref.current = setInterval(()=> { 
-      setTime(prev => prev+1)
-    }, 100)
+  useEffect(() => {
+    run();
 
-    return ()=> clearInterval(ref.current)
-  }
-    
-  }, [counting])
+    return () => clearInterval(ref.current);
+  }, [count]);
 
-  return ( 
-    <> 
-    
-    <p>{time}</p>
-    <> 
-    <button onClick={()=>setCounting(!counting)}>START</button>
-
-    {counting ? (
-    <button onClick={()=>setCounting(!counting)}>STOP</button>
-
-    ) : (
-      <button onClick={()=>setCounting(!counting)}>START</button>
-
-    ) }
+  return (
+    <>
+      <h4>time</h4>
+      <p>{count}</p>
+      <button onClick={() => clearInterval(ref.current)}>stop</button>
+      <button onClick={run}>start</button>
     </>
-    </>
-  )
-  
-
-
-
-
-  
-};  
-  
-
-
-
+  );
+};
 
 export default UseStateBasics;
