cbcc7122760bbf8b5f91b3eb0e691f7b130f0ffa
Taro Serigano <taroserigano@gmail.com>
2024-06-22 09:48:04 -0500

    Update Pagination_Beta_222_simple.jsx

--- Pagination_Beta_222_simple.jsx
+++ Pagination_Beta_222_simple.jsx
@@ -1,84 +1,86 @@
-import React, {useState, useEffect, useRef} from 'react' 
-import { data} from "../../../data" 
-// const url = "https://api.github.com/users" 
+import React, { useState, useEffect, useRef } from "react";
+import { data } from "../../../data";
 
-import axios from "axios"
-
-import Note from "./components/Note" 
-import LoginForm from './LoginForm';
+import axios from "axios";
 
 import "./style.css";
 
-import Posts from './components/Posts';
-import Pagination from './components/Pagination';
-import useFocus from  "./components/useFocus"
-import useToggle from "./components/useToggle"
-import useTimeout from "./components/useTimeout"
+import Posts from "./components/Posts";
+import Pagination from "./components/Pagination";
+import useFocus from "./components/useFocus";
+import useToggle from "./components/useToggle";
+import useTimeout from "./components/useTimeout";
 // import SearchBar from "./SearchBar"
+// const url = "https://jsonplaceholder.typicode.com/users";
+const posts = "https://jsonplaceholder.typicode.com/posts";
 
-import itemsData from "./items.json"
-const url = "https://jsonplaceholder.typicode.com/posts"
-
+const url = "https://jsonplaceholder.typicode.com/posts";
 
 const UseStateBasics = () => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [posts, setPosts] = useState([]);
+  const postsPerPage = 10;
 
-  const [currentPage, setCurrentPage] = useState(1) 
-  const [posts, setPosts] = useState([])
-  const postsPerPage = 10 
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get(url);
+      setPosts(response.data);
+    };
+    fetchData();
+  }, []);
 
-  useEffect(() => { 
-    const fetchData = async() => { 
-      const response = await axios.get(url) 
-      setPosts(response.data) 
-    }
-    fetchData() 
-  }, [])
+  const currentPageIndex = currentPage * postsPerPage;
 
-  const lastPageIndex = currentPage * postsPerPage
+  const currentPosts = posts.slice(currentPageIndex - 10, currentPageIndex);
 
-  const currentPosts = posts.slice(lastPageIndex -10, lastPageIndex)
-  
-  let totalPages = [] 
-  for(let i=1; i <= Math.ceil(posts.length / postsPerPage); i++){
-      totalPages.push(i)
+  let totalPages = [];
+  for (let i = 1; i <= Math.ceil(posts.length / postsPerPage); i++) {
+    totalPages.push(i);
   }
 
-  console.log(totalPages)
+  console.log(totalPages);
 
-  return ( 
-    <> 
-    <ul>
-    {currentPosts.map((post, idx)=> { 
-        return <li key={idx}>{post.title.substring(0, 50)} {post.title.length > 50 ? "..." : "" }</li>
-    })}
-    <p>Current Page : {currentPage} </p>
-    </ul>
-    <button onClick={()=>setCurrentPage(1)} 
-        disabled={currentPage === 1} 
-        >First</button>
-    <button onClick={()=>setCurrentPage(currentPage-1)} 
-        disabled={currentPage === 1} 
-        >Prev</button>
-    {totalPages.map((page, idx) => (
-      <button key={idx}
-      onClick={() => setCurrentPage(page)}
-      >{page}</button>
-    ))}
-        <button onClick={()=>setCurrentPage(currentPage+1)} 
+  return (
+    <>
+      <ul>
+        {currentPosts.map((post, idx) => {
+          return (
+            <li key={idx}>
+              {post.title.substring(0, 50)}{" "}
+              {post.title.length > 50 ? "..." : ""}
+            </li>
+          );
+        })}
+        <p>Current Page : {currentPage} </p>
+      </ul>
+      <button onClick={() => setCurrentPage(1)} disabled={currentPage === 1}>
+        First
+      </button>
+      <button
+        onClick={() => setCurrentPage(currentPage - 1)}
+        disabled={currentPage === 1}
+      >
+        Prev
+      </button>
+      {totalPages.map((page, idx) => (
+        <button key={idx} onClick={() => setCurrentPage(page)}>
+          {page}
+        </button>
+      ))}
+      <button
+        onClick={() => setCurrentPage(currentPage + 1)}
         disabled={currentPage === 10}
-        >Next</button>
-            <button onClick={()=>setCurrentPage(posts.length / postsPerPage)} 
-        disabled={currentPage === 10} 
-        >Last</button>
-
+      >
+        Next
+      </button>
+      <button
+        onClick={() => setCurrentPage(posts.length / postsPerPage)}
+        disabled={currentPage === 10}
+      >
+        Last
+      </button>
     </>
-  )
-
-
-};  
-  
-
-
-
+  );
+};
 
 export default UseStateBasics;
