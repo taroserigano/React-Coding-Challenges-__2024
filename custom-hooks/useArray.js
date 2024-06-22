7007a486ee5217609b64026c1824ec73e55bc00d
Taro Serigano <taroserigano@gmail.com>
2024-06-22 11:55:04 -0500

    Update useArray.js

--- custom-hooks/useArray.js
+++ custom-hooks/useArray.js
@@ -1,69 +1,25 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-const useArray = (initialArray) => {
+const useArray = (initialState) => {
+  const [array, setArray] = useState(initialState);
 
-    const [array, setArray] = useState(initialArray) 
+  const addItem = (item) => {
+    setArray([...array, item]);
+  };
 
-    const addItem = (item) => { 
-        setArray([...array, item])
-    }
+  const removeItem = (index) => {
+    setArray(array.filter((_, i) => i !== index));
+  };
 
-    const updateItem = (index, item) => {
-        const newArray = [...array] 
-        newArray[index] = item 
-        setArray(newArray) 
-     } 
-    const removeItem = (index) => {
-        const newArray = [...array] 
-        newArray.splice(index, 1) 
-        setArray(newArray) 
-     }  
+  const updateItem = (index, newItem) => {
+    setArray(array.map((item, i) => (i === index ? newItem : item)));
+  };
 
-    const clearArray = ()=> {
-        setArray([])
-     }
+  const clearArray = () => {
+    setArray([]);
+  };
 
-     return {
-        array, removeItem, updateItem, addItem, clearArray
-     }
-    
+  return { array, removeItem, updateItem, addItem, clearArray };
 };
 
 export default useArray;
-
-
-
-
-
-
-
-
-
-
-// const UseStateBasics = () => {
-
-//   const {array, addItem, removeItem, updateItem, clearArray } = useArray([1,2,3,4])
-
-//   return ( 
-//     <> 
-//     <>
-//     <button onClick={()=> addItem(99)}> Add Item</button>
-//     <button onClick={()=> removeItem(0)}> Remove</button>
-//     <button onClick={()=> updateItem(0, 777)}> Update</button>
-//     <button onClick={clearArray}> Clear</button></>
-
-
-
-// {array}
-//     </>
-//   )
- 
-
-// };
-
-  
-
-
-
-
-// export default UseStateBasics;
