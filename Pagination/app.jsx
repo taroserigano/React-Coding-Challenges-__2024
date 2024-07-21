5748da2c1ebdd06028d7ae7cb89e5b1c2a103d1b
Taro Serigano <taroserigano@gmail.com>
2024-07-21 13:36:51 -0500

    Update app.jsx

--- Pagination/app.jsx
+++ Pagination/app.jsx
@@ -1,102 +1,107 @@
-import React, {useState, useEffect} from 'react' 
-import { data} from "../../../data" 
-const url = "https://api.github.com/users" 
+import React, { useState, useEffect, useRef } from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 
-import axios from "axios"
+import { data } from "../../../data";
 
-import Note from "./Note" 
-import CreateArea from "./CreateArea" 
-import LoginForm from './LoginForm';
+import axios from "axios";
 
 import "./style.css";
 
-import Posts from './components/Posts';
-import Pagination from './components/Pagination';
+import Posts from "./components/Posts";
+import Pagination from "./components/Pagination";
+import useFocus from "./components/useFocus";
+import useToggle from "./components/useToggle";
+import useTimeout from "./components/useTimeout";
+import CreateArea from "./components/CreateArea";
+import Note from "./components/Note";
+
+import Contact from "./components/Contact";
+import Home from "./components/Home";
+import About from "./components/About";
+
+import useFetch from "./components/useFetch";
+
+import useArray from "./useArray";
 // import SearchBar from "./SearchBar"
+// const url = "https://jsonplaceholder.typicode.com/users";
+const posts = "https://jsonplaceholder.typicode.com/posts";
+
+const url = "https://jsonplaceholder.typicode.com/posts";
 
 const UseStateBasics = () => {
+  const [todos, setTodos] = useState([]);
+  const [text, setText] = useState("");
+  const [isEditing, setIsEditing] = useState(false);
+
+  const [editingID, setEditingID] = useState(null);
+
+  // write all usestates for pagination
   const [posts, setPosts] = useState([]);
   const [loading, setLoading] = useState(false);
-
   const [page, setPage] = useState(1);
   const [postsPerPage, setPostPerPage] = useState(10);
 
-  const url = "https://jsonplaceholder.typicode.com/posts"
+  const url = "https://jsonplaceholder.typicode.com/posts";
 
-  useEffect(()=> { 
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true);
+      try {
+        const response = await axios.get(url);
+        setPosts(response.data);
+        setLoading(false);
+      } catch (error) {
+        console.log(error);
+        setLoading(false);
+      }
+    };
+    fetchPosts();
+  }, []);
+  let firstPage = page - 1;
 
-    const fetchData = async() => { 
-    setLoading(true)
-      try{
-      const res = await axios.get(url) 
-      setPosts(res.data) 
-      setLoading(false)
+  let lastPage = page * postsPerPage;
 
-      } catch(err) { 
-        console.log(err)
+  console.log(firstPage, lastPage); // 0 10
+
+  const currentPosts = posts.slice(firstPage, lastPage);
+
+  const lastPost = posts.length / postsPerPage;
+
+  const handleSelect = (pageNumber) => {
+    setPage(pageNumber);
+  };
+
+  const handlePrev = () => {
+    if (page > 1) {
+      setPage(page - 1);
+    } else {
+      setPage(lastPost);
     }
+  };
+
+  const handleNext = () => {
+    if (page < lastPost) {
+      setPage(page + 1);
+    } else {
+      setPage(1);
     }
-    fetchData()
-    console.log(posts)
-  }, [])
+  };
 
-  let lastPage  = page * postsPerPage
-  let firstPage = lastPage - postsPerPage
+  // fix any possible error of below code
 
-  console.log(firstPage, lastPage)
-
-  const currentPosts = posts.slice(firstPage, lastPage) 
-
-  const lastPost = posts.length / postsPerPage 
-
-  const handleSelect = (pageNum) => { 
-    setPage(pageNum) 
-  }  
-
-  const handlePrev = () => { 
-    if(page > 1 && page <= lastPage) { 
-      setPage(page-1) 
-    } else if(page === 1){ 
-      setPage(lastPage)
-    }
-  }
-
-  const handleNext = () => { 
-    console.log(page)
-    if(page >= 1 && page < lastPost){
-      setPage(page+1)
-    } else if(page === lastPost){ 
-      setPage(1)
-    }
-  } 
-
-  return ( 
-    <> 
-    
-     <Posts posts={currentPosts} loading={loading} />
-    
-    <Pagination 
-      length = {posts.length} 
-      page={page} 
-      postsPerPage={postsPerPage}
-
-      handleSelect={handleSelect} 
-      handlePrev={handlePrev} 
-      handleNext={handleNext}
-    />
+  return (
+    <>
+      <Pagination
+        length={posts.length}
+        page={page}
+        postsPerPage={postsPerPage}
+        select={handleSelect}
+        next={handleNext}
+        prev={handlePrev}
+      />
+      <Posts posts={currentPosts} loading={loading} />
     </>
-  )
-
-
-
-
-  
-
-
-
-
-}
-
-
+  );
+};
 
 export default UseStateBasics;
